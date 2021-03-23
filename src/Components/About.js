baeb825b7b4f62b9da14a9de68d3d020c0207231
metrio <dlima.nyit@gmail.com>
2021-03-23 19:46:17 -0400

    About Complete

--- src/Components/About.js
+++ src/Components/About.js
@@ -15,38 +15,28 @@ function About() {
     return(
         <section id="AboutPage">
             <main id="AboutMe">
-                <img src={AboutMe} alt="Profile" />
-            
+                <img src={AboutMe} alt="Profile" id="profilepic"/>
                 <div className="description">
                     <p>
                         From cooking to coding, I'm a former Pastry Chef who reignited their passion for Software Engineering. I have experience developing a range of websites, online applications and animations, creating databases, and mentoring web developers.
                     </p>
-
                     <p>
                         When I'm not immersed in code,  you can find me biking around the city, staking out the best finds at the local NYC markets, and attempting to master the perfect al pastor tacos. 
                         I also love playing video and board games, live music, and spending time with the best tortieshell kitten, Anise.
                     </p>    
                 </div>
             </main>
-            <div>
-                <div className="tech-logos">
-                    Languages:
+            <div className="tech-logos">
                     <img src={JavascriptLogo} alt="JS Logo"/>
                     <img src={Ruby} alt="Ruby Logo"/>
                     <img src={CPP} alt="C++ Logo"/>
                     <img src={CSS} alt="CSS Logo"/>
                     <img src={HTMLLogo} alt="HTML Logo"/>
-                    <img src={SQL} alt="SQL Logo"/>
-                </div>
-
-                <div className="tech-logos">
-                    Frameworks:
+                    <img src={SQL} alt="SQL Logo"/>    
                     <img src={ReactLogo} alt="React logo"/>
                     <img src={Rails} alt="Rails logo"/>
                     <img src={Greensock} alt="GSAP logo"/>
-                </div>
-            </div>
-            
+             </div>
         </section>
     )
 
