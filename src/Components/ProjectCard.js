e671d36b5c396dd7f1386493c2e8f2d680e94c71
metrio <dlima.nyit@gmail.com>
2021-04-07 15:45:59 -0400

    Cleaning up CSS

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+    
+    const renderTags = () => {
+        let tags = props.projectObj.tech_tags
+
+        tags.map(ele => {
+            <li> { ele } </li>
+        })
+    }
+
 
     return(
         <div className="project-card">
-
+            <img src={props.projectObj.img} />
+            <h4>{props.projectObj.project_name}</h4>
+            
+            <ul className="tags">
+            { renderTags() }
+            </ul>
+            
+           
         </div>
     )
 }
--- src/Containers/Projects.js
+++ src/Containers/Projects.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import project_data from '../project_data.json'
+import ProjectCard from '../Components/ProjectCard'
 
 function Projects() {
+
+
+    const renderProjectCards = () => [
+        project_data.map(project => 
+            <ProjectCard projectObj={project} key={project.id} />
+        )
+    ]
+    
+
     return(
-        <h1> Projects </h1>
+        <main className="projects-page">
+            <h1 className="projectpage-declare"> Projects </h1>
+            <div className="projects-container">
+            { renderProjectCards() }
+            </div>
+            
+        </main>
     )
 }
 
